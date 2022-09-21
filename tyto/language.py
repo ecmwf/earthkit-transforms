f52655c4281fa91e728f8218718db917f5e89c66
James Varndell <james.varndell@ecmwf.int>
2022-09-21 13:36:58 +0100

    Improved list-to-human function

--- tyto/language.py
+++ tyto/language.py
@@ -1,11 +1,14 @@
 
-def list2str(
+def list_to_human(
     iterable: list,
     conjunction: str="and",
     oxford_comma: bool=False,
 ) -> str:
     list_of_strs = [str(item) for item in iterable]
-    return (
-        f"{', '.join(list_of_strs[:-1])}{', ' if oxford_comma else ' '}"
-        f"{conjunction} {list_of_strs[-1]}"
-    )
+
+    if len(list_of_strs) > 2:
+        list_of_strs = [", ".join(list_of_strs[:-1]), list_of_strs[-1]]
+        if oxford_comma:
+            list_of_strs[0] += ","
+
+    return f" {conjunction} ".join(list_of_strs)
