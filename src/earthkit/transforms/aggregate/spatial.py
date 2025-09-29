6a218b005fe5873eba3bd37a3baf49be2c29f7c3
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2025-09-29 15:50:04 +0100

    import masks

--- src/earthkit/transforms/aggregate/spatial.py
+++ src/earthkit/transforms/aggregate/spatial.py
@@ -1,4 +1,5 @@
 """Module for preservation of the legacy API, it will be removed in version 2.X of earthkit.transforms."""
-from earthkit.transforms.spatial import mask, masks, reduce
+from earthkit.transforms.spatial import mask, reduce
+from earthkit.transforms.spatial._aggregate import masks
 
 __all__ = [mask, masks, reduce]
