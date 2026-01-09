620b44373dd096a88e727f55e69dbad1d53b8502
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2026-01-09 09:28:30 +0000

    docs index

--- tests/test_30_temporal_rates.py
+++ tests/test_30_temporal_rates.py
@@ -1,9 +1,7 @@
 import numpy as np
-import pandas as pd
 import pytest
 
 # from earthkit.data.core.temporary import temp_directory
-import xarray as xr
 from earthkit import data as ek_data
 from earthkit.data.testing import earthkit_remote_test_data_file
 from earthkit.transforms import temporal
