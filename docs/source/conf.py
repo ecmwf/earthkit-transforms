f0825a6421f0f0daab910be00933a4669d2396b0
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2026-03-30 12:03:26 +0100

    faqs and why

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -154,7 +154,7 @@ html_js_files = [
     "custom.js",
 ]
 
-html_favicon = "https://raw.githubusercontent.com/ecmwf/logos/refs/heads/main/logos/earthkit/earthkit-logo-only.svg"
+html_favicon = "./_static/earthkit-transforms-notext.svg"
 
 d_thing = (
     "M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1."
